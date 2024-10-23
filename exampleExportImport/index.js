80af784cab65fcec2ccec168a4f0fb27daf7248b
HussainAzimi <hussainazimi.caree@gmail.com>
2024-10-22 19:05:10 -0500

    frist commit

---
+++ exampleExportImport/index.js
@@ -0,0 +1,5 @@
+import { printString } from "./featureOne.js";
+//import { hello, world } from "./featureTwo.js";
+import * as worlds from "./featureTwo.js";
+
+printString(worlds.hello, worlds.world);
